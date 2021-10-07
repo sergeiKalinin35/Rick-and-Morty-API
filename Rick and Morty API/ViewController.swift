ac079e44fbf4da934739281c3b79969fb2e700d4
Sergey <piolersa85@mail.ru>
2021-10-07 19:36:00 +0200

    Initial Commit

---
+++ Rick and Morty API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty API
+//
+//  Created by Sergey on 07.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
